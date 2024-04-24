f4fa96bbf080033fca9d9d01b27bf77b7e5c2bb0
Isaac Schaessens Coleman <trainee.isaac.schaessens.coleman@sigmalabs.co.uk>
2024-04-24 11:06:18 +0100

    fixed main.py

--- station_performance_pipeline/main.py
+++ station_performance_pipeline/main.py
@@ -1,8 +1,3 @@
-from os import environ as ENV
-from datetime import datetime
-
-import requests
-from requests.auth import HTTPBasicAuth
 from dotenv import load_dotenv
 
 from extract import get_station_train_services_data, train_station_arrival_data, train_station_cancellation_data
@@ -10,14 +5,3 @@ from extract import get_station_train_services_data, train_station_arrival_data,
 if __name__ == "__main__":
 
     load_dotenv()
-
-    paddington_train_services_list = get_station_train_services_data("PAD")
-
-    paddington_arrivals = train_station_arrival_data(
-        paddington_train_services_list)
-
-    paddington_cancellations = train_station_cancellation_data(
-        paddington_train_services_list)
-
-    print(len(paddington_arrivals))
-    print(len(paddington_cancellations))
