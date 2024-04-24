fad84f98e32f4bd1558874c66013f3232088616a
Isaac Schaessens Coleman <trainee.isaac.schaessens.coleman@sigmalabs.co.uk>
2024-04-24 11:09:16 +0100

    pylint score 10/10 main.py

--- station_performance_pipeline/main.py
+++ station_performance_pipeline/main.py
@@ -1,6 +1,12 @@
+"""Main script for data pipeline."""
+
 from dotenv import load_dotenv
 
-from extract import get_station_train_services_data, train_station_arrival_data, train_station_cancellation_data
+from extract import (
+    get_station_train_services_data,
+    train_station_arrival_data,
+    train_station_cancellation_data,
+)
 
 if __name__ == "__main__":
 
@@ -8,8 +14,8 @@ if __name__ == "__main__":
 
     paddington_train_services_list = get_station_train_services_data("PAD")
 
-    paddington_arrivals = train_station_arrival_data(
-        paddington_train_services_list)
+    paddington_arrivals = train_station_arrival_data(paddington_train_services_list)
 
     paddington_cancellations = train_station_cancellation_data(
-        paddington_train_services_list)
+        paddington_train_services_list
+    )
