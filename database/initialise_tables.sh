2c856a7acbb588b235da967f6ba18bc2617bb2ee
Isaac Schaessens Coleman <trainee.isaac.schaessens.coleman@sigmalabs.co.uk>
2024-04-25 13:09:30 +0100

    Added precaution measure to initialise_tables.sh script

--- database/initialise_tables.sh
+++ database/initialise_tables.sh
@@ -1,3 +1,9 @@
+read -p "Are you sure you want to run this script? (y/n): " confirm
+if [ "$confirm" != "y" ]; then
+    echo "Script execution aborted."
+    exit 1
+fi
+
 source .env
 export PGPASSWORD=$DB_PASS
 psql -h $DB_HOST -p $DB_PORT -U $DB_USER $DB_NAME -f schema.sql
