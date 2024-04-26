1b3b4e4e2a62b772a7ddfa5ae1ca066b493f6f39
Isaac Schaessens Coleman <trainee.isaac.schaessens.coleman@sigmalabs.co.uk>
2024-04-26 17:47:45 +0100

    fixed table names

--- database/historical_schema.sql
+++ database/historical_schema.sql
@@ -1,24 +1,26 @@
+DROP TABLE IF EXISTS historical_data.station_performance;
+DROP TABLE IF EXISTS historical_data.operator_performance;
 
 CREATE TABLE historical_data.station_performance(
     station_performance_id SERIAL PRIMARY KEY,
-    station_id INT REFERENCES historical_schema.stations(station_id),
-    date TIMESTAMP NOT NULL,
+    station_id INT REFERENCES public.stations(station_id),
+    day TIMESTAMP NOT NULL,
     cancellation_count INT NOT NULL,
     delay_1m_count INT NOT NULL, 
     delay_5m_count INT NOT NULL, 
     avg_delay DECIMAL(4,2) NOT NULL,
     arrival_count INT NOT NULL, 
     common_cancel_code varchar(2) 
-)
+);
 
 CREATE TABLE historical_data.operator_performance(
     operator_performance_id SERIAL PRIMARY KEY,
-    operator_id INT REFERENCES historical_schema.operators(operator_id),
-    date TIMESTAMP NOT NULL,
+    operator_id INT REFERENCES public.operators(operator_id),
+    day TIMESTAMP NOT NULL,
     cancellation_count INT NOT NULL,
     delay_1m_count INT NOT NULL, 
     delay_5m_count INT NOT NULL, 
     avg_delay DECIMAL(4,2) NOT NULL,
     arrival_count INT NOT NULL, 
     common_cancel_code varchar(2) 
-)
+);
