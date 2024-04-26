2c74c7423addc0583f904bd9acf598ef622d9ef7
Isaac Schaessens Coleman <trainee.isaac.schaessens.coleman@sigmalabs.co.uk>
2024-04-26 18:30:23 +0100

    Adding default values for some columns that need it

--- database/historical_schema.sql
+++ database/historical_schema.sql
@@ -5,10 +5,10 @@ CREATE TABLE historical_data.station_performance(
     station_performance_id SERIAL PRIMARY KEY,
     station_id INT REFERENCES public.stations(station_id),
     day DATE NOT NULL,
-    cancellation_count INT NOT NULL,
-    delay_1m_count INT NOT NULL, 
-    delay_5m_count INT NOT NULL, 
-    avg_delay DECIMAL(4,2) NOT NULL,
+    cancellation_count INT DEFAULT 0,
+    delay_1m_count INT NOT DEFAULT 0, 
+    delay_5m_count INT NOT DEFAULT 0, 
+    avg_delay DECIMAL(4,2) DEFAULT 0,
     arrival_count INT NOT NULL, 
     common_cancel_code varchar(2) 
 );
@@ -17,10 +17,10 @@ CREATE TABLE historical_data.operator_performance(
     operator_performance_id SERIAL PRIMARY KEY,
     operator_id INT REFERENCES public.operators(operator_id),
     day DATE NOT NULL,
-    cancellation_count INT NOT NULL,
-    delay_1m_count INT NOT NULL, 
-    delay_5m_count INT NOT NULL, 
-    avg_delay DECIMAL(4,2) NOT NULL,
+    cancellation_count INT DEFAULT 0,
+    delay_1m_count INT DEFAULT 0, 
+    delay_5m_count INT DEFAULT 0, 
+    avg_delay DECIMAL(4,2) DEFAULT 0,
     arrival_count INT NOT NULL, 
     common_cancel_code varchar(2) 
 );
