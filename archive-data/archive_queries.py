6ae0e89cf631b39afcf7cc4bb5ca6ba31c0cec0a
Ariba <miss-syeda@outlook.com>
2024-04-26 21:16:53 +0100

    queries for Operator_performance

--- archive-data/archive_queries.py
+++ archive-data/archive_queries.py
@@ -110,5 +110,95 @@ ORDER BY
 # Operator_performance queries
 
 O_DELAYS = """
-
+SELECT 
+    operators.operator_id, DATE(arrivals.scheduled_arrival) AS day, COUNT(*) AS delay_1m_count 
+FROM 
+    arrivals
+INNER JOIN 
+    services ON arrivals.service_id=services.service_id
+INNER JOIN 
+    operators ON services.operator_id=operators.operator_id
+WHERE 
+    arrivals.actual_arrival>arrivals.scheduled_arrival
+        AND
+    arrivals.scheduled_arrival < CURRENT_DATE - INTERVAL '7 days'
+GROUP BY 
+    operators.operator_id, day
+ORDER BY 
+    day, operators.operator_id ASC;
 """
+
+O_DELAYS_OVER_5_MIN = """
+SELECT 
+    operators.operator_id,  DATE(arrivals.scheduled_arrival) AS day, COUNT(*) AS delay_5m_count 
+FROM 
+    arrivals
+INNER JOIN 
+    services ON arrivals.service_id=services.service_id
+INNER JOIN 
+    operators ON services.operator_id=operators.operator_id
+WHERE
+    arrivals.actual_arrival > arrivals.scheduled_arrival + INTERVAL '5 minutes'
+AND
+    arrivals.scheduled_arrival < CURRENT_DATE - INTERVAL '7 days'
+GROUP BY 
+    operators.operator_id, day
+ORDER BY 
+    day, operators.operator_id ASC;
+"""
+
+O_AVG_DELAY = """
+SELECT 
+    operators.operator_id, DATE(arrivals.scheduled_arrival) AS day,
+    ROUND(AVG((EXTRACT(EPOCH FROM (actual_arrival - scheduled_arrival)) / 60)), 2) AS avg_delay_min
+FROM 
+    arrivals
+INNER JOIN 
+    services ON arrivals.service_id = services.service_id
+INNER JOIN 
+    operators ON services.operator_id = operators.operator_id
+WHERE 
+    arrivals.actual_arrival > arrivals.scheduled_arrival
+        AND 
+    arrivals.scheduled_arrival < CURRENT_DATE - INTERVAL '7 days'
+GROUP BY 
+    operators.operator_id, day
+ORDER BY 
+    day, operators.operator_id ASC;
+"""
+
+O_TOTAL_ARRIVALS = """
+SELECT 
+    operators.operator_id, DATE(arrivals.scheduled_arrival) AS day, COUNT(*) AS arrival_count
+FROM 
+    arrivals
+INNER JOIN 
+    services ON arrivals.service_id = services.service_id
+INNER JOIN 
+    operators ON services.operator_id = operators.operator_id
+WHERE 
+    arrivals.scheduled_arrival < CURRENT_DATE - INTERVAL '7 days'
+GROUP BY 
+    operators.operator_id, day
+ORDER BY 
+    day, operators.operator_id ASC;
+"""
+
+O_TOTAL_CANCELLATIONS = """
+SELECT 
+    operators.operator_id, DATE(cancellations.scheduled_arrival) AS day, COUNT(*) AS cancellation_count
+FROM 
+    cancellations
+INNER JOIN 
+    services ON cancellations.service_id = services.service_id
+INNER JOIN 
+    operators ON services.operator_id = operators.operator_id
+WHERE 
+    cancellations.scheduled_arrival < CURRENT_DATE - INTERVAL '7 days'
+GROUP BY 
+    operators.operator_id, day
+ORDER BY 
+    day, operators.operator_id ASC;
+"""
+
+O_COMMON_CANCELLATION = """ """
