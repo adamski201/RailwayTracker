a763df5b2c4a575e357d25f25b977096031e12f9
Isaac Schaessens Coleman <trainee.isaac.schaessens.coleman@sigmalabs.co.uk>
2024-04-26 18:31:53 +0100

    removed accidental NULL keywords

--- database/historical_schema.sql
+++ database/historical_schema.sql
@@ -6,8 +6,8 @@ CREATE TABLE historical_data.station_performance(
     station_id INT REFERENCES public.stations(station_id),
     day DATE NOT NULL,
     cancellation_count INT DEFAULT 0,
-    delay_1m_count INT NOT DEFAULT 0, 
-    delay_5m_count INT NOT DEFAULT 0, 
+    delay_1m_count INT DEFAULT 0, 
+    delay_5m_count INT DEFAULT 0, 
     avg_delay DECIMAL(4,2) DEFAULT 0,
     arrival_count INT NOT NULL, 
     common_cancel_code varchar(2) 
