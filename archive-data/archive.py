836742b0b0c47253c3a16563af43cd13986b65ae
Ariba <miss-syeda@outlook.com>
2024-04-29 21:55:03 +0100

    add function to delete old data and add error handling

--- archive-data/archive.py
+++ archive-data/archive.py
@@ -1,5 +1,5 @@
 """This file is responsible for moving the historical data from
-the short-term storage to the long-term storage"""
+the short-term storage to the long-term storage."""
 
 from functools import reduce
 import logging
@@ -11,20 +11,26 @@ from psycopg2.extras import RealDictCursor
 from psycopg2.extensions import connection
 import pandas as pd
 
-from archive_queries import S_DELAYS, S_DELAYS_OVER_5_MIN, S_AVG_DELAY, S_TOTAL_ARRIVALS, S_TOTAL_CANCELLATIONS, \
-    O_DELAYS, O_DELAYS_OVER_5_MIN, O_AVG_DELAY, O_TOTAL_ARRIVALS, O_TOTAL_CANCELLATIONS, INSERT_STATION_PERFORMANCE, \
-    INSERT_OPERATOR_PERFORMANCE
+from archive_queries import (S_DELAYS, S_DELAYS_OVER_5_MIN, S_AVG_DELAY, S_TOTAL_ARRIVALS, S_TOTAL_CANCELLATIONS,
+                             O_DELAYS, O_DELAYS_OVER_5_MIN, O_AVG_DELAY, O_TOTAL_ARRIVALS, O_TOTAL_CANCELLATIONS,
+                             INSERT_STATION_PERFORMANCE, INSERT_OPERATOR_PERFORMANCE,
+                             DELETE_OLD_ARRIVAL_DATA, DELETE_OLD_CANCELLATION_DATA)
+
+
+class DataFetchError(Exception):
+    """Exception raised when there is an error fetching data."""
+    pass
 
 
 def setup_logging() -> None:
-    """Sets up logging to the terminal"""
+    """Sets up the logging configuration."""
 
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s:%(levelname)s:%(message)s')
 
 
 def get_db_connection(config: dict[str, str]) -> connection:
-    """Returns a connection to a database"""
+    """Returns a connection to a database."""
 
     return connect(
         host=config["DB_HOST"],
@@ -37,62 +43,59 @@ def get_db_connection(config: dict[str, str]) -> connection:
 
 
 def close_connection(conn: connection) -> None:
-    """Closes the connection to the database"""
+    """Closes the connection to the database."""
 
     conn.close()
 
 
-def fetch_and_delete_data(conn: connection, fetch_query: str, delete_query: str) -> pd.DataFrame:
-    """Fetches historical data based on the fetch_query
-    and then deletes the fetched data using the delete_query.
-    Returns the fetched data as a DataFrame"""
+def fetch_old_data(conn: connection, query: str) -> pd.DataFrame:
+    """Fetches historical data based on the query.
+    Returns the fetched data as a DataFrame."""
 
     try:
+
         with conn.cursor() as cur:
-            cur.execute("BEGIN;")
-
-            cur.execute(fetch_query)
+            cur.execute(query)
             rows = cur.fetchall()
-
-            cur.execute(delete_query)
-
-            conn.commit()
         return pd.DataFrame(rows)
 
-    except psycopg2.Error as err:
+    except Error as err:
+
         conn.rollback()
-        logging.error("An error occurred: %s", err)
+        logging.error("Failed to fetch data: %s", err)
         if err.pgcode:
             logging.error("Error code: %s", err.pgcode)
-        if err.pgerror:
-            logging.error("Detailed error: %s", err.pgerror)
 
 
 def get_stations_performance(conn: connection, queries: list[str]) -> pd.DataFrame:
-    """Fetches the historical data for the stations based on the queries"""
+    """Fetches the historical data for the stations based on the queries."""
 
     performance_data = []  # list for adding the dataframes
 
     for query in queries:
-        result = fetch_and_delete_data(conn, query)
-        print("done")
-        if result is not None:
+        result = fetch_old_data(conn, query)
+        if not result.empty:
             performance_data.append(result)
+
+    if not performance_data:
+        raise DataFetchError("No data fetched from the database.")
 
     # combines the dataframes
     return reduce(lambda left, right: pd.merge(left, right, on=['day', 'station_id'], how='outer'), performance_data)
 
 
 def get_operators_performance(conn: connection, queries: list[str]) -> pd.DataFrame:
-    """Fetches the historical data for the operators based on the queries"""
+    """Fetches the historical data for the operators based on the queries."""
 
     performance_data = []  # list for adding the dataframes
 
     for query in queries:
-        result = fetch_and_delete_data(conn, query)
-        print("done")
-        if result is not None:
+        result = fetch_old_data(conn, query)
+        if not result.empty:
             performance_data.append(result)
+
+    if not performance_data:
+        raise DataFetchError("No data fetched from the database.")
 
     # combines the dataframes
     return reduce(lambda left, right: pd.merge(left, right, on=['day', 'operator_id'], how='outer'), performance_data)
@@ -100,7 +103,7 @@ def get_operators_performance(conn: connection, queries: list[str]) -> pd.DataFr
 
 def clean_data(data: pd.DataFrame) -> pd.DataFrame:
     """Cleans the data by converting the columns to the
-    correct data types and filling in the missing values"""
+    correct data types and filling in the missing values."""
 
     data['day'] = pd.to_datetime(data['day']).dt.date
     data['delay_1m_count'] = data['delay_1m_count'].fillna(0).astype(int)
@@ -112,26 +115,46 @@ def clean_data(data: pd.DataFrame) -> pd.DataFrame:
 
 
 def convert_to_list(df: str) -> list[tuple]:
-    """Converts a DataFrame to a list of tuples"""
+    """Converts a DataFrame to a list of tuples."""
 
     return df.to_records(index=False).tolist()
 
 
 def load_to_db(conn: connection, data: list[tuple], query: str) -> None:
-    """Adds the data to the database"""
+    """Loads the data to the database using the query provided."""
 
     try:
+
         with conn.cursor() as cur:
             cur.executemany(query, data)
         conn.commit()
 
-    except psycopg2.Error as err:
+    except Error as err:
+
         conn.rollback()
-        logging.error("An error occurred: %s", err)
+        logging.error("Failed to load data: %s", err)
         if err.pgcode:
             logging.error("Error code: %s", err.pgcode)
-        if err.pgerror:
-            logging.error("Detailed error: %s", err.pgerror)
+
+
+def delete_old_data(conn: connection, queries: list[str]):
+    """Deletes old data from 'arrivals' and 'cancellations'
+    tables in the database that are older than 30 days."""
+
+    try:
+
+        with conn.cursor() as cur:
+            cur.execute("BEGIN;")
+            for query in queries:
+                cur.execute(query)
+            conn.commit()
+
+    except Error as err:
+
+        conn.rollback()
+        logging.error("Failed to delete old data: %s", err)
+        if err.pgcode:
+            logging.error("Error code: %s", err.pgcode)
 
 
 def save_to_csv(data: pd.DataFrame, filename: str) -> None:
@@ -153,10 +176,13 @@ if __name__ == "__main__":
     stations_data = clean_data(get_stations_performance(conn, station_queries))
     operators_data = clean_data(get_operators_performance(conn, operator_queries))
 
-    # save_to_csv(stations_data, 'stations_data.csv')
-    # save_to_csv(operators_data, 'operators_data.csv')
+    save_to_csv(stations_data, 'stations_data.csv')
+    save_to_csv(operators_data, 'operators_data.csv')
 
     load_to_db(conn, convert_to_list(stations_data), INSERT_STATION_PERFORMANCE)
     load_to_db(conn, convert_to_list(stations_data), INSERT_OPERATOR_PERFORMANCE)
+
+    deletion_queries = [DELETE_OLD_ARRIVAL_DATA, DELETE_OLD_CANCELLATION_DATA]
+    delete_old_data(conn, deletion_queries)
 
     close_connection(conn)
