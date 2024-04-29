5d9aa7bb0863f7b7c572a70891fa9d00da6a28de
Isaac Schaessens Coleman <trainee.isaac.schaessens.coleman@sigmalabs.co.uk>
2024-04-29 14:44:11 +0100

    Deleted freq canncellation column and rearranged columns

--- database/historical_schema.sql
+++ database/historical_schema.sql
@@ -1,26 +1,24 @@
-DROP TABLE IF EXISTS historical_data.station_performance;
-DROP TABLE IF EXISTS historical_data.operator_performance;
+DROP TABLE IF EXISTS archive.station_performance;
+DROP TABLE IF EXISTS archive.operator_performance;
 
 CREATE TABLE historical_data.station_performance(
     station_performance_id SERIAL PRIMARY KEY,
     station_id INT REFERENCES public.stations(station_id),
     day DATE NOT NULL,
-    cancellation_count INT DEFAULT 0,
     delay_1m_count INT DEFAULT 0, 
     delay_5m_count INT DEFAULT 0, 
     avg_delay_min DECIMAL(4,2) DEFAULT 0,
     arrival_count INT NOT NULL, 
-    freq_cancellation_type_id INT REFERENCES public.cancellation_types(cancellation_type_id)
+    cancellation_count INT DEFAULT 0
 );
 
-CREATE TABLE historical_data.operator_performance(
+CREATE TABLE archive.operator_performance(
     operator_performance_id SERIAL PRIMARY KEY,
     operator_id INT REFERENCES public.operators(operator_id),
     day DATE NOT NULL,
-    cancellation_count INT DEFAULT 0,
     delay_1m_count INT DEFAULT 0, 
     delay_5m_count INT DEFAULT 0, 
     avg_delay_min DECIMAL(4,2) DEFAULT 0,
     arrival_count INT NOT NULL, 
-    freq_cancellation_type_id INT REFERENCES public.cancellation_types(cancellation_type_id)
+    cancellation_count INT DEFAULT 0
 );
