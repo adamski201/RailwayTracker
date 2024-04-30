46be290472891478421d1497c99fe8fef98584fb
saniya <saniyashaikh@live.co.uk>
2024-04-30 17:31:52 +0100

    updated incident transform test script

--- incidents/test_transform.py
+++ incidents/test_transform.py
@@ -3,47 +3,39 @@
 import pandas as pd
 from pandas.testing import assert_frame_equal
 
-from transform import process_xml, replace_new_line, convert_timestamps
+from transform import process_xml, convert_timestamps
 from test_data.test_vars import (OUTPUT_AFTER_PROCESSING_XML,
-                                 OUTPUT_AFTER_REPLACE,
                                  TIMESTAMP_WITH_END_TIME,
                                  TIMESTAMP_WITHOUT_END_TIME,
                                  TIMESTAMP_OUTPUT_1,
                                  TIMESTAMP_OUTPUT_2)
 
 UK_TIMEZONE = 'Europe/London'
+NS = {"ns": "http://nationalrail.co.uk/xml/incident",
+      "com": "http://nationalrail.co.uk/xml/common"}
+
 
 with open("incidents/test_data/test_incident_data.xml", "r", encoding="utf-8") as file:
     MSG = file.read()
 
 
 def test_process_xml_normal_input():
-    """Tests the process xml function if input contains expected string"""
+    """Tests the process xml function if input contains expected string."""
 
-    processed_msg = process_xml(MSG)
+    processed_msg = process_xml(MSG, NS)
     assert processed_msg == OUTPUT_AFTER_PROCESSING_XML
-    assert isinstance(processed_msg, list)
+    assert isinstance(processed_msg, dict)
 
 
 def test_process_xml_bad_input():
-    """Tests process xml function if input string is empty"""
+    """Tests process xml function if input string is empty."""
 
     empty_msg = ''
-    assert process_xml(empty_msg) is None
-
-
-def test_replace_new_line_normal_input():
-    """Tests replace new line character function"""
-
-    processed_msg = process_xml(MSG)
-    data_frame = pd.DataFrame(processed_msg)
-    formatted_msg = replace_new_line(data_frame)
-    expected_df = pd.DataFrame(OUTPUT_AFTER_REPLACE)
-    assert_frame_equal(formatted_msg, expected_df)
+    assert process_xml(empty_msg, NS) is None
 
 
 def test_convert_timestamps_with_end_time_normal_input():
-    """Tests converting timestamps with all timestamps"""
+    """Tests converting timestamps with all timestamps."""
 
     timestamps = pd.DataFrame(TIMESTAMP_WITH_END_TIME)
     converted = convert_timestamps(
@@ -53,7 +45,7 @@ def test_convert_timestamps_with_end_time_normal_input():
 
 
 def test_convert_timestamps_without_end_time_normal_input():
-    """Tests converting timestamps without end time"""
+    """Tests converting timestamps without end time."""
 
     timestamps = pd.DataFrame(TIMESTAMP_WITHOUT_END_TIME)
     converted = convert_timestamps(timestamps, UK_TIMEZONE)
