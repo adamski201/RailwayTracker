079f530bb1c34496f824ce5177ab3e5134127ac0
Ariba <miss-syeda@outlook.com>
2024-05-02 23:24:39 +0100

    updates variables

--- terraform/archive/variables.tf
+++ terraform/archive/variables.tf
@@ -9,3 +9,27 @@ variable "AWS_SECRET_ACCESS_KEY" {
 variable "AWS_REGION" {
   type = string
 }
+
+variable "DB_HOST" {
+  type = string
+}
+
+variable "DB_PORT" {
+  type = string
+}
+
+variable "DB_USER" {
+  type = string
+}
+
+variable "DB_PASS" {
+  type = string
+}
+
+variable "DB_NAME" {
+  type = string
+}
+
+variable "S3_BUCKET" {
+  type = string
+}
