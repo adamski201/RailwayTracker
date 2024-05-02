f8c6a21796e1f51be4574b2544a67237666e0d72
Ariba <miss-syeda@outlook.com>
2024-05-02 23:15:17 +0100

    shuffled variables so it aligns with the other files

--- terraform/s3_bucket/variables.tf
+++ terraform/s3_bucket/variables.tf
@@ -1,11 +1,12 @@
+variable "AWS_REGION" {
+  type    = string
+  default = "eu-west-2"
+}
+
 variable "AWS_ACCESS_KEY_ID" {
   type = string
 }
 
 variable "AWS_SECRET_ACCESS_KEY" {
-  type = string
-}
-
-variable "AWS_REGION" {
   type = string
 }
